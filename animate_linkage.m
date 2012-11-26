a56fc7e6f015b8317991dcafb97040c51499dba8
wrongu <richard.d.lange.13@dartmouth.edu>
2012-11-26 16:32:52 -0500

    plot results just animates the end result

--- animate_linkage.m
+++ animate_linkage.m
@@ -2,7 +2,7 @@ function [] = animate_linkage(L)
     [v tr] = simulate_rotation(L);
     if v
         m = traj_to_movie(tr);
-        movie(m, 5, 15);
+        movie(m, 50, 15);
     else
         disp('invalid dimensions');
     end
